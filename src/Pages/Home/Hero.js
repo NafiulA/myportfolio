da6996a6cf725c2594e6d3664e257b3a4163dee7
Nafiul Alam <nafiulalam30@gmail.com>
2022-06-01 20:48:50 +0600

    hero image placement

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import portfolio from "../../assets/images/portfolio.png";
 
 const Hero = () => {
-    return (<div>
+    return (
+        <div className='min-h-screen bg-[#2D2E32]'>
+            <div className='w-full md:w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 justify-center items-center'>
+                <div className='order-2 lg:order-1'>
+                    <div className='w-60 bg-[#57C78E] rounded-md'>
 
-    </div>
+                    </div>
+                </div>
+                <div className='order-1 lg:order-2'>
+                    <div className='static h-min mt-20 lg:mt-0 lg:relative lg:h-screen'>
+                        <img className='static lg:absolute lg:bottom-0' src={portfolio} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
