9c72aff6751f6ea5747b00f72ab6d831c7d70b16
Nafiul Alam <nafiulalam30@gmail.com>
2022-07-03 21:41:51 +0600

    delete option for admins

---
+++ src/Pages/Dashboard/AdminDltModal.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import toast from 'react-hot-toast';
+
+const AdminDltModal = (dltModal, setDltModal, refetch) => {
+    const handleDelete = () => {
+        fetch(`https://blooming-badlands-86390.herokuapp.com//admin/reviews/${dltModal._id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json",
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            },
+
+        }).then(res => res.json())
+            .then(data => {
+                if (data.deletedCount) {
+                    setDltModal(null);
+                    toast.success("Testimony Deleted", { id: "testimonyDelete" });
+                    refetch();
+                }
+            })
+
+    }
+
+    return (
+        <div>
+            <input type="checkbox" id="myTestimonyDltModal" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box bg-[#2D2E32]">
+                    <h3 className="font-bold text-lg text-gray-400">Are you sure you want to delete this testimony?</h3>
+                    <div className="modal-action">
+                        <button onClick={handleDelete} className='py-1 px-3 rounded text-white bg-[#57c78e]'>Yes</button>
+                        <label onClick={() => setDltModal(null)} htmlFor="myTestimonyDltModal" className="p-1 rounded text-white bg-red-500">Cancel</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminDltModal;
--- src/Pages/Dashboard/AllTestimonials.js
+++ src/Pages/Dashboard/AllTestimonials.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import MytestimoniesRow from './MytestimoniesRow';
+import MyTestimonyDltModal from './MyTestimonyDltModal';
 
 const AllTestimonials = () => {
+    const [adminDltModal, setAdminDltModal] = useState(null);
+    const { data: testimonies, isLoading, refetch } = useQuery("testimonies", () => fetch(`https://blooming-badlands-86390.herokuapp.com/reviews`, {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
-        <div>
-
+        <div className='p-5'>
+            <div className="overflow-x-auto rounded-xl">
+                <table className="border-collapse table-auto text-center lg:w-full">
+                    <thead className='bg-[#2D2E32] text-gray-400'>
+                        <tr>
+                            <th className='w-16 px-4'>#</th>
+                            <th className='px-4'>Name</th>
+                            <th className='px-4'>Testimony</th>
+                            <th className='px-4'>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {testimonies.map((testimony, index) => <MytestimoniesRow key={testimony._id}
+                            testimony={testimony} index={index} setDltModal={setAdminDltModal}></MytestimoniesRow>)}
+                    </tbody>
+                </table>
+            </div>
+            {adminDltModal && <MyTestimonyDltModal dltModal={adminDltModal} setDltModal={setAdminDltModal} refetch={refetch}></MyTestimonyDltModal>}
         </div>
     );
 };
